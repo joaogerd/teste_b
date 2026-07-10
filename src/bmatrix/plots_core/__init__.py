f0982b17dcdb22db89dbb3284793d82c412a3f9a
João Gerd Zell de Mattos <joao.gerd@gmail.com>
2026-07-10 19:06:43 -0300

    Apply left-aligned title layout to plots

--- src/bmatrix/plots_core/__init__.py
+++ src/bmatrix/plots_core/__init__.py
@@ -1,5 +1,7 @@
 """Plotting stage for MPAS-JEDI/SABER B-matrix products."""
 
 from . import map_enhancements as _map_enhancements
+from . import title_layout as _title_layout
 
 _map_enhancements.apply()
+_title_layout.apply()
